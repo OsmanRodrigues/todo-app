b5afef8a55fb2a4a0b933255fe072f0a81b8791b
OsmanRodrigues <osmancesar.mr@gmail.com>
2021-03-05 12:06:03 -0300

    initial commit

---
+++ todo-backend/src/index.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { AddressInfo } from 'net';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Test!!!' });
+});
+
+const server = app.listen(process.env.APP_PORT, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
